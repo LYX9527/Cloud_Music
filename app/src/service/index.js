91701fb1f0f8602f84ac3554f337c85af254c4b8
yilantingfeng <lyxing9527@163.com>
2022-04-09 21:22:44 +0800

    update

--- app/src/service/index.js
+++ app/src/service/index.js
@@ -7,7 +7,7 @@ import cache from '@/service/cache'
 axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 axios.defaults.withCredentials = true;
 const service = axios.create({
-    baseURL: "https://124.223.203.125:11222/",
+    baseURL: "http://124.223.203.125:11222/",
     timeout: 10000
 })
 // request拦截器
